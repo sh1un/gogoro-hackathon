8a86c8d3398ef56ee9bd92e79b89b9961fe930c9
sh1un <a0923183408@gmail.com>
2024-05-16 02:14:01 +0800

    feat: add main.py file with initial bedrock code

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain_aws import ChatBedrock
+
+load_dotenv()
+
+
+def get_model(model: str = "claude 3 sonnet") -> ChatBedrock:
+    model = model.lower()
+    if model == "claude 3 sonnet":
+        llm = ChatBedrock(
+            credentials_profile_name="amb-shiun-bedrock-user",
+            model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+            streaming=True,
+        )
+
+    return llm
+
+
+if __name__ == "__main__":
+    llm = get_model()
+    response = llm.invoke("hello world!")
+    print(response)
