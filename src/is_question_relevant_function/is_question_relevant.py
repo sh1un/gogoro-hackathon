b537cb97c03944ff0d9c95a70d74c8554e246718
sh1un <a0923183408@gmail.com>
2024-05-18 13:42:37 +0800

    refactor(is_question_relevant.py): remove unused functions and imports

--- src/is_question_relevant_function/is_question_relevant.py
+++ src/is_question_relevant_function/is_question_relevant.py
@@ -90,36 +90,6 @@ def create_opensearch_vector_search_client(
     return docsearch
 
 
-def create_index(opensearch_client, index_name):
-    settings = {"settings": {"index": {"knn": True, "knn.space_type": "cosinesimil"}}}
-    response = opensearch_client.indices.create(index=index_name, body=settings)
-    return bool(response["acknowledged"])
-
-
-def create_index_mapping(opensearch_client, index_name):
-    response = opensearch_client.indices.put_mapping(
-        index=index_name,
-        body={
-            "properties": {
-                "vector_field": {"type": "knn_vector", "dimension": 1536},
-                "text": {"type": "keyword"},
-            }
-        },
-    )
-    return bool(response["acknowledged"])
-
-
-def delete_opensearch_index(opensearch_client, index_name):
-    logger.info(f"Trying to delete index {index_name}")
-    try:
-        response = opensearch_client.indices.delete(index=index_name)
-        logger.info(f"Index {index_name} deleted")
-        return response["acknowledged"]
-    except Exception as e:
-        logger.info(f"Index {index_name} not found, nothing to delete")
-        return True
-
-
 def lambda_handler(event, context):
     logger.info("Starting...")
 
